05ebeb7b320ce26b58c4ea2a698146d5ede2dded
brendanng7 <brendanngjj@gmail.com>
2024-01-08 03:38:09 +0800

    Homepage completed, added typescript config

---
+++ app/javascript/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hero() {
+    return (
+        <>
+            <div className="circle">
+                <img className="circle-image" src="tofu.jpg" alt="Logo" />
+            </div>
+        </>
+    )
+};
+
+export default Hero;
---
+++ app/javascript/components/Home.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ResponsiveAppBar from "./Navbar";
+import Hero from "./Hero";
+
+function Home() {
+    return (
+        <div className="page-white">
+            <ResponsiveAppBar />
+            <div className="vh-90 d-flex align-items-center justify-content-center">
+                <div className="jumbotron jumbotron-fluid flex-column">
+                    <Hero />
+                    <div className="d-flex justify-content-center m-4">
+                        <Link role="button" to={"/register"} className="btn custom-button">Register</Link>
+                        <Link role="button" to={"/login"} className="btn custom-button">Login</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
